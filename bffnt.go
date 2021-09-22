b2476378ef34bbce797e140ea63da80847b8ebf3
paeyn <paeynivek@gmail.com>
2021-09-22 14:19:20 -0500

    Finish decoding all headers
    Authored-by: paeyn <paeynivek@gmail.com>

--- bffnt.go
+++ bffnt.go
@@ -32,7 +32,7 @@ type CFNT struct { //       Offset  Size  Description
 	BlockReadNum  uint32 // 0x10    0x04  Number of "blocks" to read
 }
 
-func (cfnt CFNT) decode(raw []byte) {
+func (cfnt *CFNT) decode(raw []byte) {
 	cfnt.MagicHeader = string(raw[0:4])
 	cfnt.Endianness = fmt.Sprintf("%X", binary.BigEndian.Uint16(raw[4:]))
 	cfnt.SectionSize = binary.BigEndian.Uint16(raw[6:])
@@ -66,7 +66,7 @@ type FINF_BFFNT struct { //  Offset  Size  Description
 	CMAPOffset     uint32 // 0x1C    0x04  CMAP Offset
 }
 
-func (finf FINF_BFFNT) decode(raw []byte) {
+func (finf *FINF_BFFNT) decode(raw []byte) {
 	finf.MagicHeader = string(raw[0:4])
 	finf.SectionSize = binary.BigEndian.Uint32(raw[4:])
 	finf.FontType = raw[8] // byte == uint8
@@ -87,6 +87,104 @@ func (finf FINF_BFFNT) decode(raw []byte) {
 	pprint(finf)
 }
 
+type TGLP_BFFNT struct { //    Offset  Size  Description
+	MagicHeader      string // 0x00    0x04  Magic Header (TGLP)
+	SectionSize      uint32 // 0x04    0x04  Section Size
+	CellWidth        uint8  // 0x08    0x01  Cell Width
+	CellHeight       uint8  // 0x09    0x01  Cell Height
+	NumOfSheets      uint8  // 0x0A    0x01  Number of Sheets
+	MaxCharWidth     uint8  // 0x0B    0x01  Max Character Width
+	SheetSize        uint32 // 0x0C    0x04  Sheet Size
+	BaselinePosition uint16 // 0x10    0x02  Baseline Position
+	SheetImageFormat uint16 // 0x12    0x02  Sheet Image Format 0-13: (RGBA8, RGB8, RGBA5551, RGB565, RGBA4, LA8, HILO8, L8, A8, LA4, L4, A4, ETC1, ETC1A4)
+	NumOfColumns     uint16 // 0x14    0x02  Number of Sheet columns
+	NumOfRows        uint16 // 0x16    0x02  Number of Sheet rows
+	SheetWidth       uint16 // 0x18    0x02  Sheet Width
+	SheetHeight      uint16 // 0x1A    0x02  Sheet Height
+	SheetDataOffset  uint32 // 0x1C    0x04  Sheet Data Offset
+}
+
+func (tglp *TGLP_BFFNT) decode(raw []byte) {
+	tglp.MagicHeader = string(raw[0:4])
+	tglp.SectionSize = binary.BigEndian.Uint32(raw[4:])
+	tglp.CellWidth = raw[8] // byte == uint8
+	tglp.CellHeight = raw[9]
+	tglp.NumOfSheets = raw[10]
+	tglp.MaxCharWidth = raw[11]
+	tglp.SheetSize = binary.BigEndian.Uint32(raw[12:])
+	tglp.BaselinePosition = binary.BigEndian.Uint16(raw[12:])
+	tglp.SheetImageFormat = binary.BigEndian.Uint16(raw[14:])
+	tglp.NumOfColumns = binary.BigEndian.Uint16(raw[16:])
+	tglp.NumOfRows = binary.BigEndian.Uint16(raw[18:])
+	tglp.SheetWidth = binary.BigEndian.Uint16(raw[20:])
+	tglp.SheetHeight = binary.BigEndian.Uint16(raw[22:])
+	tglp.SheetDataOffset = binary.BigEndian.Uint32(raw[24:])
+
+	fmt.Println("TGLP Header")
+	pprint(tglp)
+}
+
+type CWDH struct { //           Offset  Size                             Description
+	MagicHeader string //       0x00    0x04                             Magic Header (CWDH)
+	SectionSize uint32 //       0x04    0x04                             Section Size
+	// StartIndex     uint16 // 0x08    0x02                             Start Index
+	// EndIndex       uint16 // 0x0A    0x02                             End Index
+	// NextCDWHOffset uint32 // 0x0C    0x04                             Next CWDH Offset
+	// LeftWidth      uint8  // 0x10    3 * (EndIndex - StartIndex + 1)  Char Widths (3 bytes: Left, Glyph Width, Char Width)
+	// GlyphWidth     uint8
+	// CharWidth      uint8
+
+	// First glyph included to keep things in line with the documentation
+	glyphInfo
+}
+
+type glyphInfo struct {
+	StartIndex     uint16
+	EndIndex       uint16
+	NextCWDHOffset uint32
+	LeftWidth      uint8
+	GlyphWidth     uint8
+	CharWidth      uint8
+}
+
+func (cwdh *CWDH) decode(raw []byte) []glyphInfo {
+	cwdh.MagicHeader = string(raw[0:4])
+	cwdh.SectionSize = binary.BigEndian.Uint32(raw[4:])
+	cwdh.StartIndex = binary.BigEndian.Uint16(raw[8:])
+	cwdh.EndIndex = binary.BigEndian.Uint16(raw[10:])
+	cwdh.NextCWDHOffset = binary.BigEndian.Uint32(raw[12:])
+	cwdh.LeftWidth = raw[16]
+	cwdh.GlyphWidth = raw[17]
+	cwdh.CharWidth = raw[18]
+
+	fmt.Println("CWDH Header")
+	pprint(cwdh)
+	return nil
+}
+
+type CMAP struct { //         Offset  Size  Description
+	MagicHeader     string // 0x00    0x04  Magic Header (CMAP)
+	SectionSize     uint32 // 0x04    0x04  Section Size
+	CodeBegin       uint16 // 0x08    0x02  Code Begin
+	CodeEnd         uint16 // 0x0A    0x02  Code End
+	MappingMethod   uint16 // 0x0C    0x02  Mapping Method (0 = Direct, 1 = Table, 2 = Scan)
+	UnknownReserved uint16 // 0x0E    0x02  Reserved?
+	NextCMAPOffset  uint32 // 0x10    0x04  Next CMAP Offset
+}
+
+func (cmap *CMAP) decode(raw []byte) {
+	cmap.MagicHeader = string(raw[0:4])
+	cmap.SectionSize = binary.BigEndian.Uint32(raw[4:])
+	cmap.CodeBegin = binary.BigEndian.Uint16(raw[8:])
+	cmap.CodeEnd = binary.BigEndian.Uint16(raw[10:])
+	cmap.MappingMethod = binary.BigEndian.Uint16(raw[12:])
+	cmap.UnknownReserved = binary.BigEndian.Uint16(raw[14:])
+	cmap.NextCMAPOffset = binary.BigEndian.Uint32(raw[16:])
+
+	fmt.Println("CMAP Header")
+	pprint(cmap)
+}
+
 // This BFFNT file is Breath of the Wild's NormalS_00.bffnt. The goal of the
 // project is to create a bffnt encoder/decoder so I can upscale this font
 const testBffntFile = "NormalS_00.bffnt"
@@ -98,65 +196,17 @@ func main() {
 	var cfnt CFNT
 	cfnt.decode(rawBytes[0:20])
 
-	fmt.Println()
-
 	var finf FINF_BFFNT
 	finf.decode(rawBytes[20:52])
-}
 
-// WIP
+	var tglp TGLP_BFFNT
+	tglp.decode(rawBytes[52:84])
 
-// Offset  Size  Description
-// 0x00    0x04  Magic Header (TGLP)
-// 0x04    0x04  Section Size
-// 0x08    0x01  Cell Width
-// 0x09    0x01  Cell Height
-// 0x0A    0x01  Number of Sheets
-// 0x0B    0x01  Max Character Width
-// 0x0C    0x04  Sheet Size
-// 0x10    0x02  Baseline Position
-// 0x12    0x02  Sheet Image Format 0-13: (RGBA8, RGB8, RGBA5551, RGB565, RGBA4, LA8, HILO8, L8, A8, LA4, L4, A4, ETC1, ETC1A4)
-// 0x14    0x02  Number of Sheet columns
-// 0x16    0x02  Number of Sheet rows
-// 0x18    0x02  Sheet Width
-// 0x1A    0x02  Sheet Height
-// 0x1C    0x04  Sheet Data Offset
-type TGLP_BFFNT struct {
-	MagicHeader      string
-	SectionSize      uint32
-	CellWidth        uint8
-	CellHeight       uint8
-	NumOfSheets      uint8
-	MaxCharWidth     uint8
-	SheetSize        uint32
-	BaselinePosition uint16
-	SheetImageFormat uint16
-	NumOfColumns     uint16
-	NumOfRows        uint16
-	SheetWidth       uint16
-	SheetHeight      uint16
-	SheetDataOffset  uint32
-}
+	var cwdh CWDH
+	// CWDHOffset skips the first 8 bytes that contain the CWDH Magic Header
+	cwdh.decode(rawBytes[finf.CWDHOffset-8:])
 
-func (t TGLP_BFFNT) decode(raw []byte) {
-	// temporary variables
-	a := raw[0:4]
-	// b := binary.BigEndian.Uint32(raw[4:])
-	// c := binary.BigEndian.Uint8(raw[6:])
-	// d := binary.BigEndian.Uint8(raw[6:])
-	// e := binary.BigEndian.Uint8(raw[6:])
-	// f := binary.BigEndian.Uint8(raw[6:])
-	// g := binary.BigEndian.Uint32(raw[8:])
-	// h := binary.BigEndian.Uint32(raw[12:])
-	// i := binary.BigEndian.Uint16(raw[16:])
-	// j := binary.BigEndian.Uint16(raw[16:])
-	// k := binary.BigEndian.Uint16(raw[16:])
-	// l := binary.BigEndian.Uint16(raw[16:])
-	// m := binary.BigEndian.Uint16(raw[16:])
-	// n := binary.BigEndian.Uint16(raw[16:])
-	// o := binary.BigEndian.Uint32(raw[16:])
-
-	t.MagicHeader = string(a)
-
-	pprint(t)
+	var cmap CMAP
+	// CMAPOffset skips the first 8 bytes that contain the CWDH Magic Header
+	cmap.decode(rawBytes[finf.CMAPOffset-8:])
 }
